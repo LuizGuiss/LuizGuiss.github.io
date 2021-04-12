2ce6a8a119e49da2c12f4407ccd653673438159e
MBicalho <77859700+MBicalho@users.noreply.github.com>
2021-04-12 14:06:01 -0300

    Adicionado colisões, pontuação, menu

--- jogo/assets/jogo.js
+++ jogo/assets/jogo.js
@@ -47,94 +47,231 @@ function setupControls() {
 
 //arvores
 
-var templateTreeLeft;
-var templateTreeCenter;
-var templateTreeRight;
+var templateObstaculoEsquerda;
+var templateObstaculoCentro;
+var templateObstaculoDireita;
 
-var treeContainer;
-var numberOfTrees = 0;
+var obstaculoContainer;
+var numeroDeObstaculos = 0;
 
 var templates;
 
-function setupTrees() {
-  templateTreeLeft = document.getElementById('template-tree-left');
-  templateTreeCenter = document.getElementById('template-tree-center');
-  templateTreeRight = document.getElementById('template-tree-right');
-  treeContainer = document.getElementById('tree-container');
+function setupObstaculos() {
+  templateObstaculoEsquerda = document.getElementById('template-obstaculo-esquerda');
+  templateObstaculoCentro = document.getElementById('template-obstaculo-centro');
+  templateObstaculoDireita = document.getElementById('template-obstaculo-direita');
+  obstaculoContainer = document.getElementById('obstaculo-container');
 
-  templates = [templateTreeLeft, templateTreeCenter, templateTreeRight];
+  templates = [templateObstaculoEsquerda, templateObstaculoCentro, templateObstaculoDireita];
 
-  removeTree(templateTreeLeft);
-  removeTree(templateTreeCenter);
-  removeTree(templateTreeRight);
+  removeObstaculo(templateObstaculoEsquerda);
+  removeObstaculo(templateObstaculoCentro);
+  removeObstaculo(templateObstaculoDireita);
 }
 
-function removeTree(tree) {
-  tree.parentNode.removeChild(tree);
+function removeObstaculo(obstaculo) {
+  obstaculo.parentNode.removeChild(obstaculo);
 }
 
-function addTree(el) {
-  numberOfTrees += 1;
-  el.id = 'tree-' + numberOfTrees;
-  treeContainer.appendChild(el);
+function addObstaculo(el) {
+  numeroDeObstaculos += 1;
+  el.id = 'obstaculo-' + numeroDeObstaculos;
+  obstaculoContainer.appendChild(el);
 }
 
-function addTreeTo(position_index) {
+function addObstaculoTo(position_index) {
   var template = templates[position_index];
-  addTree(template.cloneNode(true));
+  addObstaculo(template.cloneNode(true));
 }
 
-function addTreesRandomly(
+function addObstaculosRandomly(
   {
-    probTreeLeft = 0.5,
-    probTreeCenter = 0.5,
-    probTreeRight = 0.5,
-    maxNumberTrees = 2
+    probObstaculoEsquerda = 0.5,
+    probObstaculoCentro = 0.5,
+    probObstaculoDireita = 0.5,
+    maxnumeroObstaculos = 2
   } = {}) {
-    var trees = [
-      {probability: probTreeLeft, position_index: 0},
-      {probability: probTreeCenter, position_index: 1},
-      {probability: probTreeRight, position_index: 2},
-    ];
-    shuffle(trees);
+  var obstaculos = [
+    { probability: probObstaculoEsquerda, position_index: 0 },
+    { probability: probObstaculoCentro, position_index: 1 },
+    { probability: probObstaculoDireita, position_index: 2 },
+  ];
+  shuffle(obstaculos);
 
-    var numberOfTreesAdded = 0;
-    trees.forEach(function (tree) {
-      if(Math.random() < tree.probability && numberOfTreesAdded < maxNumberTrees) {
-        addTreeTo(tree.position_index);
-        numberOfTreesAdded += 1;
-      }
-    }); 
+  var numeroDeObstaculosAdded = 0;
+  obstaculos.forEach(function (obstaculo) {
+    if (Math.random() < obstaculo.probability && numeroDeObstaculosAdded < maxnumeroObstaculos) {
+      addObstaculoTo(obstaculo.position_index);
+      numeroDeObstaculosAdded += 1;
+    }
+  });
 
-    return numberOfTreesAdded;
-  }
+  return numeroDeObstaculosAdded;
+}
 
-  var treeTimer;
+var obstaculoTimer;
 
-  function teardownnTrees() {
-    clearInterval(treeTimer);
-  }
+function zeraObstaculos() {
+  clearInterval(obstaculoTimer);
+}
 
-  function addTreesRandomlyLoop({intervalLength = 750} = {}) {
-    treeTimer = setInterval(addTreesRandomly, intervalLength);
-  }
+function addObstaculosRandomlyLoop({ intervalLength = 750 } = {}) {
+  obstaculoTimer = setInterval(addObstaculosRandomly, intervalLength);
+}
+
+//colisões
+const POSICAO_SEM_VISAO = 1;
+const POSICAO_INICIO_DE_LINHA = 0.6;
+const POSICAO_FINAL_DE_LINHA = 0.7;
+
+function setupColisoes() {
+  AFRAME.registerComponent('player', {
+    tick: function () {
+      document.querySelectorAll('.obstaculo').forEach(function (obstaculo) {
+        position = obstaculo.getAttribute('position');
+        obstaculo_position_index = obstaculo.getAttribute('data-obstaculo-position-index');
+        obstaculo_id = obstaculo.getAttribute('id');
+
+        if (position.z > POSICAO_SEM_VISAO) {
+          removeObstaculo(obstaculo);
+        }
+
+        if (!isGameRunning) return;
+
+        if (POSICAO_INICIO_DE_LINHA < position.z && position.z < POSICAO_FINAL_DE_LINHA
+          && obstaculo_position_index == player_position_index) {
+          gameOver();
+
+          if (position.z > POSICAO_FINAL_DE_LINHA) {
+            addPontuacaoParaObstaculo(obstaculo_id);
+            atualizaExibePontuacao();
+          }
+        }
+      })
+    }
+  })
+}
+
+//pontuação
+
+var pontuacao;
+var contadorObstaculo;
+var exibePontuacao;
+var gameOverExibePontuacao;
+
+function setupPontuacao() {
+  pontuacao = 0;
+  contadorObstaculo = new Set();
+  exibePontuacao = document.getElementById('score');
+  gameOverExibePontuacao = document.getElementById('game-score');
+}
+
+function zeraPontuacao() {
+  exibePontuacao.setAttribute('value', '');
+  gameOverExibePontuacao.setAttribute('value', pontuacao);
+}
+
+function addPontuacaoParaObstaculo(obstaculo_id) {
+  if (contadorObstaculo.has(obstaculo_id)) return;
+  pontuacao = pontuacao + 1;
+  contadorObstaculo.add(obstaculo_id);
+}
+
+function atualizaExibePontuacao() {
+  exibePontuacao.setAttribute('value', pontuacao);
+}
+
+//menu
+
+var menuStart;
+var menuGameOver;
+var menuContainer;
+var isGameRunning = false;
+var startButton;
+var restartButton;
+
+function ocultaEntity(el) {
+  el.setAttribute('visible', false);
+}
+
+function exibeEntity(el) {
+  el.setAttribute('visible', true);
+}
+
+
+function setupTodosMenus() {
+  menuStart     = document.getElementById('start-menu');
+  menuGameOver  = document.getElementById('game-over');
+  menuContainer = document.getElementById('menu-container');
+  startButton   = document.getElementById('start-button');
+  restartButton = document.getElementById('restart-button');
+
+  startButton.addEventListener('click', startGame);
+  restartButton.addEventListener('click', startGame);
+
+  exibeStartMenu();
+}
+
+function ocultarTodosMenus() {
+  ocultaEntity(menuContainer);
+  startButton.classList.remove('clickable');
+  restartButton.classList.remove('clickable');
+}
+
+function exibirGameOverMenu() {
+  exibeEntity(menuContainer);
+  ocultaEntity(menuStart);
+  exibeEntity(menuGameOver);
+  startButton.classList.remove('clickable');
+  restartButton.classList.add('clickable');
+}
+
+function exibeStartMenu(){
+  exibeEntity(menuContainer);
+  ocultaEntity(menuGameOver);
+  exibeEntity(menuStart);
+  startButton.classList.add('clickable');
+  restartButton.classList.remove('clickable');
+}
 
 //game
 
+var isGameRunning = false;
+
 setupControls();
+setupColisoes();
 
 window.onload = function () {
-  setupTrees();
+  setupTodosMenus();
+  setupPontuacao();
+  setupObstaculos();
 
-  addTreesRandomlyLoop();
 }
 
+function startGame() {
+  if (isGameRunning) return;
+  isGameRunning = true;
+
+  setupPontuacao();
+  atualizaExibePontuacao();
+  addObstaculosRandomlyLoop();
+  ocultarTodosMenus();
+}
+
+function gameOver() {
+  isGameRunning = false;
+
+  alert('Game Over!');
+  zeraObstaculos();
+  zeraPontuacao();
+  exibirGameOverMenu();
+}
 
 // utilities
 
 function shuffle(a) {
   var j, x, i;
-  for(i = a.length -1; i > 0; i --){
+  for (i = a.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
     x = a[i];
     a[i] = a[j];
